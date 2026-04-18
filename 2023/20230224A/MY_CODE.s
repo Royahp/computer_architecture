7f80fcf7162fa513468575ece4952acab3a83c55
Royahp <roya89.hosseinpour@gmail.com>
2026-04-18 11:25:25 +0200

    2024 started

--- 2023/20230224A/MY_CODE.s
+++ 2023/20230224A/MY_CODE.s
@@ -1,62 +1,113 @@
- 
-           RN  0
-		   RN  1
-		   RN  2
-		   RN  3
-		   RN  4
-		   RN  5
-		   RN  6
-		   RN  7
-		   RN  8
-		   RN  10
-		   RN  11
-		   
-		   
-		   
-		   AREA MYCODE,CODE,READONLY
-			   
-NAME           PROC
-	           EXPORT NAME
-			   PUSH{R4-R8,R10-R11,LR}
-		   
-		   
-		   
-		   
-		   
-		   
-		   
-		   
-		   
-		   MOV R0,RESULT
-		   
-		     POP{R4-R8,R10-R11,PC}
-			 ENDP
-			 END 
-				 
-				 
-;ADD  PNTR, SP, #32 ;stack pointer	
-;STR  NUM, [PNTR]        ;  store digit IN STACK
-;ADD  PNTR, PNTR, #4     ;  next slot
-;LDR 	TMP,[PNTR] ;GET DIGIT FROM STACK
-;LSL A,B,#1 A=2B
-;CS,HS unsigned>=
-;CC,LO unsigned<
-;MI negative
-;PL positive or 0
-;VS overFlow VC no over
-;HI unsigned>
-;LS unsigned<=
-;GE signed>=
-;LE signed <=
-;GT igned>
-;LT signed <
-;AL always
-;MVN Rd, Rn ,ADDS RD,#1 ; MAKUS MIKONE YANI AGE Rn = 0000 1101 MISHE Rd = 1111 0010 
-;X=UL  MVN U,U - MVN L,L -ADDS L,L,#1 -ADC U,U,#0 ;MAKUSE ADADE DO BAKHSI BA CARRY BIT
-;{SUBS  TMP, E2, E1      ; TMP = E2 - E1  
-;IT    MI               ; agar hasele tafrigh manfi bud,mosbatesh kone
-;RSBMI TMP, TMP, #0   }  ; TMP = 0 - TMP  => abs
-;BIC X, X,mask ;baraye 0 kardane bit morede nazar masalan mask=0101 bashe bit0 va bit2 ra tuye x sefr mikone
+A          RN 0
+TEN        RN 1
+Q          RN 2
+BB         RN 3
+TMP        RN 4
+NUM        RN 5
+DIG1       RN 6
+DIG2       RN 7
+DIG3       RN 8
+DIG4       RN 9
+C          RN 10
 
-;lowercase letter 97-122
-;upper 65-90
+        AREA MYCODE, CODE, READONLY
+
+KaprekarRoutine PROC
+        EXPORT KaprekarRoutine
+        PUSH {R4-R8,R10-R11,LR}
+
+        MOV NUM, A
+        LDR TEN, =10
+
+        ; -------- ??????? ?????? --------
+
+        UDIV Q, NUM, TEN
+        MUL TMP, Q, TEN
+        SUB DIG1, NUM, TMP
+
+        MOV NUM, Q
+        UDIV Q, NUM, TEN
+        MUL TMP, Q, TEN
+        SUB DIG2, NUM, TMP
+
+        MOV NUM, Q
+        UDIV Q, NUM, TEN
+        MUL TMP, Q, TEN
+        SUB DIG3, NUM, TMP
+
+        MOV NUM, Q
+        UDIV Q, NUM, TEN
+        MUL TMP, Q, TEN
+        SUB DIG4, NUM, TMP
+
+        ; -------- ????????? ????? --------
+
+        CMP DIG1, DIG2
+        BGE SKIP1
+        MOV TMP, DIG1
+        MOV DIG1, DIG2
+        MOV DIG2, TMP
+SKIP1
+
+        CMP DIG1, DIG3
+        BGE SKIP2
+        MOV TMP, DIG1
+        MOV DIG1, DIG3
+        MOV DIG3, TMP
+SKIP2
+
+        CMP DIG1, DIG4
+        BGE SKIP3
+        MOV TMP, DIG1
+        MOV DIG1, DIG4
+        MOV DIG4, TMP
+SKIP3
+
+        CMP DIG2, DIG3
+        BGE SKIP4
+        MOV TMP, DIG2
+        MOV DIG2, DIG3
+        MOV DIG3, TMP
+SKIP4
+
+        CMP DIG2, DIG4
+        BGE SKIP5
+        MOV TMP, DIG2
+        MOV DIG2, DIG4
+        MOV DIG4, TMP
+SKIP5
+
+        CMP DIG3, DIG4
+        BGE SKIP6
+        MOV TMP, DIG3
+        MOV DIG3, DIG4
+        MOV DIG4, TMP
+SKIP6
+       
+	   MUL TMP,DIG1,TEN
+	   ADD TMP,DIG2
+	   MUL TMP,TEN
+	   ADD TMP,DIG3
+	   MUL TMP,TEN
+	   ADD TMP,DIG4
+	   MOV BB,TMP
+	   
+	    MUL TMP,DIG4,TEN
+	   ADD TMP,DIG3
+	   MUL TMP,TEN
+	   ADD TMP,DIG2
+	   MUL TMP,TEN
+	   ADD TMP,DIG1
+	   MOV C,TMP
+	   
+	   SUB TMP,BB,C
+	   
+	   MOV R0,TMP
+	   
+
+      
+
+        POP {R4-R8,R10-R11,PC}
+        ENDP
+
+        END
--- 2023/20230224A/RTE/Device/LPC1768/startup_LPC17xx.s
+++ 2023/20230224A/RTE/Device/LPC1768/startup_LPC17xx.s
@@ -127,10 +127,13 @@ CRP_Key         DCD     0xFFFFFFFF
 
 Reset_Handler   PROC
                 EXPORT  Reset_Handler             [WEAK]
-				IMPORT NAME
-                IMPORT  __main
-                LDR     R0, =__main
-                BX      R0
+				IMPORT KaprekarRoutine 
+				LDR R0,=3075
+					
+				BL KaprekarRoutine 
+;                IMPORT  __main
+;                LDR     R0, =__main
+;                BX      R0
 				;SYSTICK
 ;				LDR r0, =SYScontrolAndStatusReg
 ;				MOV r1, #0
