75ca4a1c86c1cb4b66886a3027eabc412ffb0fe5
Royahp <roya89.hosseinpour@gmail.com>
2026-06-20 21:53:31 +0200

    call 2b

---
+++ second/2026/20260203A2/MY_CODE.s
@@ -0,0 +1,110 @@
+ 
+NUM        RN  0
+TEN		   RN  1
+Q		   RN  2
+TMP		   RN  3
+REM		   RN  4
+PRE		   RN  5
+N		   RN  6
+CNT		   RN  7
+;		   RN  8
+;		   RN  10
+;		   RN  11
+;		   
+		   
+		   
+		   AREA MYCODE,CODE,READONLY
+			   
+Look_and_say   PROC
+	           EXPORT Look_and_say
+			   PUSH{R4-R8,R10-R11,LR}
+		   
+		       LDR TEN,=10
+			   LDR PRE,=0
+			   
+FOR			   
+               UDIV Q,NUM,TEN
+			   MUL TMP,Q,TEN
+			   SUB REM,NUM,TMP
+			   
+			   MUL PRE,TEN
+			   ADD PRE,REM
+			   MOV NUM,Q
+			   CMP NUM,#0
+			   BNE FOR
+
+               MOV NUM,PRE
+			   LDR N,=0
+			   LDR CNT,=1
+			   
+    		   UDIV Q,NUM,TEN
+			   MUL TMP,Q,TEN
+			   SUB REM,NUM,TMP		
+     
+               MOV PRE,REM
+			   MOV NUM,Q
+
+
+			   
+FOR2		   
+               
+               UDIV Q,NUM,TEN
+			   MUL TMP,Q,TEN
+			   SUB REM,NUM,TMP	
+			   MOV NUM,Q
+               
+               CMP PRE,REM
+               ITT EQ
+               ADDEQ CNT,#1
+			   BEQ FOR2
+			   
+			   MUL N,TEN
+			   ADD N,CNT
+			   MUL N,TEN
+			   ADD N,PRE
+			   MOV PRE,REM
+			   LDR CNT,=1
+			   CMP NUM,#0
+			   BNE FOR2
+			   
+              			   
+		   
+		   
+		   
+		   
+		   
+FINISH		   
+		   
+		      MOV R0,N
+		   
+		     POP{R4-R8,R10-R11,PC}
+			 ENDP
+			 END 
+				 
+				 
+;ADD  PNTR, SP, #32 ;stack pointer	
+;STR  NUM, [PNTR]        ;  store digit IN STACK
+;ADD  PNTR, PNTR, #4     ;  next slot
+;LDR 	TMP,[PNTR] ;GET DIGIT FROM STACK
+;LSL A,B,#1 A=2B
+;CS,HS unsigned>=
+;CC,LO unsigned<
+;MI negative
+;PL positive or 0
+;VS overFlow VC no over
+;HI unsigned>
+;LS unsigned<=
+;GE signed>=
+;LE signed <=
+;GT igned>
+;LT signed <
+;AL always
+;MVN Rd, Rn ,ADDS RD,#1 ; MAKUS MIKONE YANI AGE Rn = 0000 1101 MISHE Rd = 1111 0010 
+;X=UL  MVN U,U - MVN L,L -ADDS L,L,#1 -ADC U,U,#0 ;MAKUSE ADADE DO BAKHSI BA CARRY BIT
+;{SUBS  TMP, E2, E1      ; TMP = E2 - E1  
+;IT    MI               ; agar hasele tafrigh manfi bud,mosbatesh kone
+;RSBMI TMP, TMP, #0   }  ; TMP = 0 - TMP  => abs
+;BIC X, X,mask ;baraye 0 kardane bit morede nazar masalan mask=0101 bashe bit0 va bit2 ra tuye x sefr mikone
+
+;lowercase letter 97-122
+;upper 65-90
